d493f0eb25a5fb6dd8d059a180c8d66399557e64
Berkay Kasin <berkaykasin@gmail.com>
2026-03-25 18:38:40 +0000

    everything

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,31 @@ def build_rag(pdf):
 
 vector_database = build_rag("EU_AI_ACT_2024.pdf")
 
-example = vector_database.similarity_search("requirements for high-risk AI systems?", k=3)
-print(example)
+    # example = vector_database.similarity_search("requirements for high-risk AI systems?", k=3)
+    # print(example[0].page_content)
+
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=st.secrets["GEMINI_API_KEY"])
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=vector_database.as_retriever(search_kwargs={"k": 3}),
+        return_source_documents=True
+    )
+
+    response = qa_chain.invoke({"query": query})
+    return response["result"]
+
+
+st.set_page_config(page_title="RAG EU Regulation ChatBot", layout="wide")
+
+st.title("RAG EU Regulation ChatBot")
+st.write("EU AI Act chatbot")
+
+user_query = st.text_input("Enter Your Message Here", placeholder="e.g. What are the transparency obligations for AI providers? ")
+
+if st.button("Send"):
+    if user_query:
+        st.write(f"Getting a response from AI from the database...")
+        response = get_llm_reply(user_query)
+        st.write(f"{response}")
+    else:
+        st.warning("Please enter a message")
